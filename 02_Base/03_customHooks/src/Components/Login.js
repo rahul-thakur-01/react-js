5855864f3992e9282705f30a265e68feecc4fe5a
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-01 19:02:48 +0530

    fix some error

--- 02_Base/03_customHooks/src/Components/Login.js
+++ 02_Base/03_customHooks/src/Components/Login.js
@@ -2,7 +2,7 @@ import {  useState } from "react";
 import useLocalStorage from "./useLocalStorage";
 export default function Login(){
   
-   const [password, setPassword] = useState(""); 
+const [password, setPassword] = useState(""); 
 
   //  const [email,setEmail] = useState("");
 
